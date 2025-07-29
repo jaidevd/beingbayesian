bd5393bb50d4057ec004029aef038508aae32e63
deshpande.jaidev@gmail.com <deshpande.jaidev@gmail.com>
2025-07-29 19:38:27 +0530

    ENH: Finish the analysis

--- 03-90-hr-workweek/load_hh.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-import pandas as pd
-df = pd.read_parquet('../../time-use-survey-2024/clean/hh-tus-2024.parquet')
-to_remove = ['schedule_id', 'schedule', 'survey_year',
-             'nss_region', 'stratum', 'sub_stratum',
-             'sub_round', 'fod_sub_region', 'informant_sl_no']
-df.drop(to_remove, axis=1, inplace=True)
